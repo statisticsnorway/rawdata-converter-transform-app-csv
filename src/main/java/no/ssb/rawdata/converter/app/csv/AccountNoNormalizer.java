649f853dcbfa4e290a88d74def96529c60b39250
fnh-ssb <fnh@ssb.no>
2021-06-02 19:23:18 +0200

    fix

--- src/main/java/no/ssb/rawdata/converter/app/csv/AccountNoNormalizer.java
+++ src/main/java/no/ssb/rawdata/converter/app/csv/AccountNoNormalizer.java
@@ -10,13 +10,14 @@ import no.ssb.avro.convert.core.FieldDescriptor;
 @Slf4j
 public class AccountNoNormalizer {
     @NonNull
-    private final String accountNoFieldName;
+    private final String accountNoFieldName = "BrukerstedNavn";
 
     public String checkAndFix(FieldDescriptor field, String varValue) {
-        if (accountNoFieldName.equals(field.getName()) && varValue != null && varValue.length() > 11) {
-            // log.debug("Field value: {}", varValue);
+        if (accountNoFieldName.equals(field.getName()) && (varValue.contains("\n") || varValue.contains(System. lineSeparator())
+                || varValue.contains("\t\n"))) {
+            log.debug("Field value: {}", varValue);
             // TODO: do something with field, like:
-            return varValue.substring(0, varValue.length()-1);
+            return varValue.replace("\n", "").replace("\t\n"," ").replace(System.lineSeparator(), " ");
         }
         ;
         return varValue;
